9494721432b206cc7cd16783bb5eb5e1efb5beef
ray-vazquez <rayvazquez125@gmail.com>
2020-03-26 18:49:19 -0400

    adding arrays.js

---
+++ exercises/01.arrays.js
@@ -0,0 +1,27 @@
+/////////////////////////
+//////// Arrays /////////
+/////////////////////////
+
+/**
+ * @see  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+/**
+ * @description
+ * create an arrays of names
+ */
+
+/**
+ * @description
+ * access the first item in the array above
+ */
+
+/**
+ * @description
+ * access the last character in the first item in the array above
+ */
+
+/**
+ * @do
+ * include this file in a new pull request
+ */
