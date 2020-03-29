9dd3c62fb49db5741e573c386447c69ec9e7cad0
ray-vazquez <rayvazquez125@gmail.com>
2020-03-29 13:11:41 -0400

    added arrays challenge exersice

---
+++ exercises/02.strings.js
@@ -0,0 +1,33 @@
+/////////////////////////
+///// More Strings //////
+/////////////////////////
+
+/**
+ * @description
+ * Create a vairable called fullName. Set its vaule to your full name.
+ */
+
+var fullName = "";
+
+/**
+ * @description
+ *
+ * create a substring from fullName that contains the value of your first name. Store the result in a variable
+ */
+
+/**
+ * @description
+ * access the last letter of fullName. store the result in a variable. Remember that data structure numbering is index based
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+ */
+
+/**
+ * @description
+ * create a substring from the last 5 characters
+ */
+
+/**
+ * @do
+ * include this file in a new commit
+ */
---
+++ exercises/03.arrays-challenge.js
@@ -0,0 +1,84 @@
+/////////////////////////
+///// More Arrays ///////
+/////////////////////////
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+/**
+ * @description
+ * create and array with 5 names
+ */
+
+/**
+ * @description
+ * use a for loop to create a copy of the names array
+ */
+
+/**
+ * @description
+ * add a sixth name to the second index position of the names array.
+ * store your resultes in a new array
+ * /
+
+/**
+ * @description
+ * use a for loop to title case each state in the array below
+ *  * ie: 'alabama' becomes 'Alabama', 'new hampshire' becomes 'New Hampshire'
+ *
+ * save your results to a new array
+ */
+
+let states = [
+  "alabama",
+  "alaska",
+  "arizona",
+  "arkansas",
+  "california",
+  "colorado",
+  "connecticut",
+  "delaware",
+  "florida",
+  "georgia",
+  "hawaii",
+  "idaho",
+  "illinois",
+  "indiana",
+  "iowa",
+  "kansas",
+  "kentucky",
+  "louisiana",
+  "maine",
+  "maryland",
+  "massachusetts",
+  "michigan",
+  "minnesota",
+  "mississippi",
+  "missouri",
+  "montana",
+  "nebraska",
+  "nevada",
+  "new hampshire",
+  "new jersey",
+  "new mexico",
+  "new york",
+  "north carolina",
+  "north dakota ",
+  "ohio",
+  "oklahoma",
+  "oregon",
+  "pennsylvania",
+  "rhode island",
+  "south carolina",
+  "south dakota",
+  "tennessee",
+  "texas",
+  "utah",
+  "vermont",
+  "virginia",
+  "washington",
+  "west virginia",
+  "wisconsin",
+  "wyoming"
+];
